111ce20c10807baeecd119275f9382bcb8e0de63
ajisawa <ajisawa@arsweb.co.jp>
2021-04-20 12:43:49 +0900

    added strage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "ap-northeast-1",
     "aws_user_pools_id": "ap-northeast-1_rpKSUDbCm",
     "aws_user_pools_web_client_id": "3iqo60gdsrkuikkn5mrmebbvg1",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://ehojeqkfrveyhd7zz4x2fp5igu.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-2cr5vft6d5ajtmw5aupquk2hlq",
+    "aws_user_files_s3_bucket": "foodstock-aji15753-staging",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
